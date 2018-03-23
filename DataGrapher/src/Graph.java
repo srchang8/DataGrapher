897d9156820811cddb6150f2f0ed99dc82159124
stephen chang <stephenchang@stephens-MacBook-Pro.local>
2018-03-23 15:25:12 -0400

    add settings and draw y components

--- DataGrapher/src/Graph.java
+++ DataGrapher/src/Graph.java
@@ -1,4 +1,39 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.List;
 
-public class Graph {
+import javax.swing.JPanel;
+
+public class Graph extends JPanel{
+	
+	GraphSettings settings = new GraphSettings();
+	
+	public void drawYaxis(Graphics2D graphic) {
+	
+		int distanceBetweenYValues = 0;
+		System.out.println(settings.graphHeight / settings.numberYDivisions);
+		
+		int yCounter = settings.padding;
+		int yAxisLabelValue = 100;
+		
+		for (int i=0; i <= settings.numberYDivisions; i++) {
+			
+			//draw long y lines
+			graphic.setColor(settings.gridColor);
+			graphic.drawLine(settings.padding, yCounter, settings.graphWidth, yCounter);
+			
+			//draw short y lines
+			graphic.setColor(Color.BLACK);
+			graphic.drawLine(settings.padding, yCounter, settings.padding, yCounter);
+			
+			//label y values on the graph with cpu percentage string
+			String yAxisValuesString = Integer.toString(yAxisLabelValue);
+			graphic.drawString(yAxisValuesString + "%", settings.padding - 35, yCounter + 5);
+			yAxisLabelValue = yAxisLabelValue - 10;
+			
+			yCounter = yCounter + distanceBetweenYValues;
+			
+		}
+	}
 
 }
---
+++ DataGrapher/src/GraphSettings.java
@@ -0,0 +1,19 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Stroke;
+
+
+public class GraphSettings {
+	
+	public int padding = 50;
+	public Color lineColor = new Color(44, 102, 230, 180);
+	public Color pointColor = new Color(100, 100, 100, 180);
+	public Color gridColor = new Color(200, 200, 200, 200);
+	public static final Stroke GRAPH_STROKE = new BasicStroke(2f);
+	public int numberYDivisions = 10;
+	public int lengthOfYLines = 5;
+	public int graphWidth = 0;
+	public int graphHeight = 0;
+	
+
+}
